8239d0b33cbe2651982239a6da8a3da2e7fa7d23
SuddenGunter <kolomytsev1996@gmail.com>
2024-10-31 19:28:44 +0000

    top level api

--- src/edynamojson.erl
+++ src/edynamojson.erl
@@ -1,12 +1,22 @@
 -module(edynamojson).
 
 -export([
-    serialize/1,
-    deserialize/1
+    serialize_document/1,
+    deserialize_document/1
 ]).
+
+serialize_document(Obj) when is_map(Obj) ->
+    serialize(Obj);
+serialize_document(_Obj) ->
+    error(invalid_document_type).
+
+deserialize_document(Obj) when is_map(Obj) ->
+    deserialize(Obj);
+deserialize_document(_Obj) ->
+    error(invalid_document_type).
 
 serialize(Obj) ->
     error(not_implemented).
 
-deserialize(_Obj) ->
+deserialize(Obj) ->
     error(not_implemented).
