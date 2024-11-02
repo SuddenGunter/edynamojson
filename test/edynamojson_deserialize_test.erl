2242413916ab0d0037c396bc4deae28a0e9d6012
SuddenGunter <kolomytsev1996@gmail.com>
2024-11-02 23:00:45 +0000

    validation

--- test/edynamojson_deserialize_test.erl
+++ test/edynamojson_deserialize_test.erl
@@ -4,6 +4,38 @@
 
 -record(dynamo_msg, {id, value}).
 
+invalid_document_type_test_() ->
+    [?_test(invalid_document_type_base(fun(X) -> X end)),
+     ?_test(invalid_document_type_base(self())),
+     ?_test(invalid_document_type_base(atom)),
+     ?_test(invalid_document_type_base(make_ref())),
+     ?_test(invalid_document_type_base({1, 2, 3})),
+     ?_test(invalid_document_type_base(#dynamo_msg{id = 123, value = 321})),
+     ?_test(invalid_document_type_base(<<"str">>)),
+     ?_test(invalid_document_type_base("str")),
+     ?_test(invalid_document_type_base(123)),
+     ?_test(invalid_document_type_base([123])),
+     ?_test(invalid_document_type_base(true))].
+
+invalid_document_type_base(Input) ->
+    ?assertException(error, invalid_document_type, edynamojson:deserialize_term(Input)).
+
+invalid_map_key_type_test_() ->
+    [?_test(invalid_map_key_type_base(fun(X) -> X end)),
+     ?_test(invalid_map_key_type_base(self())),
+     ?_test(invalid_map_key_type_base(atom)),
+     ?_test(invalid_map_key_type_base(make_ref())),
+     ?_test(invalid_map_key_type_base({1, 2, 3})),
+     ?_test(invalid_map_key_type_base(#dynamo_msg{id = 123, value = 321})),
+     ?_test(invalid_map_key_type_base("str")),
+     ?_test(invalid_map_key_type_base(123)),
+     ?_test(invalid_map_key_type_base([123])),
+     ?_test(invalid_map_key_type_base(true))].
+
+invalid_map_key_type_base(Key) ->
+    Input = #{Key => <<"test_field">>},
+    ?assertException(error, invalid_map_key_type, edynamojson:deserialize_term(Input)).
+
 deserialize_term_test_() ->
     [?_test(deserialize_term_base(#{}, #{})),
      ?_test(deserialize_term_base(#{<<"A">> => #{<<"S">> => <<"B">>}}, #{<<"A">> => <<"B">>})),
